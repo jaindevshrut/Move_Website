63637f98d78900800a16020b9403021ab597576d
Dev Shrut Jain <138303636+jaindevshrut@users.noreply.github.com>
2025-02-08 20:03:11 +0530

    Added the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+// we are unable to return mutiple elements in react so we use Fragment (<> </>) to wrap all the elements
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>This is a paragraph</p>
+    </>
+  )
+}
+
+export default App
